de2a457aea0f0c9d494c9275621cf4fb8e039439
Gefen <gefenbar009@gmail.com>
2022-07-07 17:31:15 +0300

    fix gameplay

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const winCombinations = [['0', '1', '2'], ['0', '3', '6'], ['3', '4', '5'], ['6', '7', '8'], ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']]
-player = []
-
 initGame()
 function initGame() {
     turns = 0
+    player = []
     playerX = []
     playerO = []
+    win = false
 }
 
 function mark(tile) {
-    if (tile.textContent === '-') {
+    if (tile.textContent === '-' && !win) {
         if (turns % 2 === 0) {
             tile.textContent = "X"
-            playerX.push(tile.id)
+            playerX.push(tile.name)
             player = playerX
         }
         else {
             tile.textContent = "0"
-            playerO.push(tile.id)
+            playerO.push(tile.name)
             player = playerO
         }
-        checkWin(player)
         turns++
+        checkWin(player)
     }
 }
 
@@ -33,11 +33,16 @@ function checkWin(player) {
                 if (player[k] === winCombinations[i][j]) {
                     match++
                     if (match === 3) {
+                        win = true
                         if (player === playerX)
                             player = "X"
                         else
                             player = "O"
                         document.getElementById('score').innerText = player + " is the WINNER "
+                        rematch()
+                    }
+                    if (turns == 9) {
+                        document.getElementById('score').innerText = "It's a DRAW! "
                         rematch()
                     }
                 }
@@ -47,7 +52,6 @@ function checkWin(player) {
 }
 
 function rematch() {
-    document
     const reset = document.createElement('button')
     reset.textContent = 'Reset'
     document.getElementById('score').appendChild(reset)
@@ -55,7 +59,7 @@ function rematch() {
         for (let i = 0; i < 9; i++) {
             document.getElementsByClassName('tiles')[i].textContent = '-'
         }
-        document.getElementById('score').innerText = ''
+        document.getElementById('score').innerText = 'X vs O'
         reset.prepend
         initGame()
     }
